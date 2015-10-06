23ebd29e5396d4ebc7eaba34eb97cc93fa5a434c
Aaron Jacobs <atheriel@gmail.com>
2015-10-06 18:08:43 -0400

    Adds colours, an axis, and an overlay to the spacewalk viz.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- spacewalks/spacewalks.js
+++ spacewalks/spacewalks.js
@@ -17,7 +17,8 @@ function ready(error, data) {
 	return {
 	    id: +row.ID,
 	    date: d3.time.format("%Y-%m-%d").parse(row.Date),
-	    duration: +row.Duration
+	    duration: +row.Duration,
+	    country: row.Country
 	};
     });
 
@@ -45,12 +46,36 @@ function ready(error, data) {
 	.attr("height", chart.height)
 	.append("g");
 
+    // Define the arrow glyph.
+    d3.select("svg").append('defs')
+	.append('marker')
+	.attr("id", "Triangle")
+	.attr("refX", 2)
+	.attr("refY", 3)
+	.attr("markerUnits", "userSpaceOnUse")
+	.attr("markerWidth", 6)
+	.attr("markerHeight", 9)
+	.attr("orient", "auto")
+	.append("path")
+	.style("fill", "#ccc")
+	.style("shape-rendering", "geometricPrecision")
+	.attr("d", "M 0 0 6 3 0 6 1.5 3");
+
     var bars = chart.svg.selectAll(".chart-bar")
 	.data(chart.data);
 
     bars.enter()
 	.append("rect")
-	.attr("class", "chart-bar")
+	.attr("class", function(d) {
+	    switch(d.country) {
+	    case "USA":
+		return "chart-bar chart-bar-usa";
+	    case "Russia":
+		return "chart-bar chart-bar-russia";
+	    default:
+		return "chart-bar";
+	    }
+	})
 	.attr("y", function(d) { return chart.timeScale(d.id); })
 	.attr("width", function(d) {
 	    return - chart.heightScale(d.duration) + chart.heightScale(0);
@@ -59,6 +84,37 @@ function ready(error, data) {
 	    return chart.heightScale(d.duration);
 	})
 	.attr("height", 10);
+
+    var durationAxis = chart.svg.append("g")
+	.classed("chart-axis", true);
+
+    durationAxis.append("line")
+	.attr("x1", 350)
+	.attr("y1", chart.timeScale(1) - 15)
+	.attr("x2", 200)
+	.attr("y2", chart.timeScale(1) - 15)
+	.attr("marker-end", "url(#Triangle)");
+
+    durationAxis.append("text")
+	.text("Duration of Spacewalk")
+	.attr("x", 355)
+	.attr("y", chart.timeScale(1) - 15)
+	.attr("dy", "0.2rem");
+
+    // var overlay = chart.svg.selectAll(".chart-bar-overlay")
+    // 	.data(chart.data);
+
+    // overlay.enter()
+    // 	.append("rect")
+    // 	.attr("class", "chart-bar-overlay")
+    // 	.attr("y", function(d) { return chart.timeScale(d.id); })
+    // 	.attr("width", function(d) {
+    // 	    return chart.heightScale(0) - 25;
+    // 	})
+    // 	.attr("x", function(d) {
+    // 	    return 25;
+    // 	})
+    // 	.attr("height", 10);
 
     debug = chart;
 }
